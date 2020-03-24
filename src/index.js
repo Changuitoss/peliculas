3d713a48b4640359f2dc754060f3ca99fdb47833
Changuitoss <ezealvarezrubio@gmail.com>
2020-03-23 23:04:29 -0300

    Ok busqueda, trae resultados

--- src/index.js
+++ src/index.js
@@ -43,31 +43,77 @@ function limpiarHindis(peliculasTodas) {
 
 function obtenerPeliculas(e) {
   e.preventDefault()
-  const genero = e.target.genero.value
-  const desdeAno = e.target.anodesde.value
-  const hastaAno = e.target.anohasta.value
-  const puntaje = e.target.puntajedesde.value
+  const tipo = e.target.tipo.value;
+  const genero = e.target.genero.value;
+  const desdeAno = e.target.anodesde.value;
+  const hastaAno = e.target.anohasta.value;
+  const puntaje = e.target.puntajedesde.value;
 
-  const url =  `https://api.themoviedb.org/3/discover/movie?api_key=e4c325cfe50ba68791f7165086f631e4&
-                sort_by=vote_average.desc&include_adult=false&include_video=false&
-                page=1&
-                ?with_genres=${genero}&
-                primary_release_date.gte=${desdeAno}-01-01&
-                primary_release_date.lte=${hastaAno}-12-12&
-                vote_count.gte=1000&
-                vote_average.gte=${puntaje}`
+  const url =  `https://api.themoviedb.org/3/discover/${tipo}?api_key=e4c325cfe50ba68791f7165086f631e4&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&with_genres=${genero}&primary_release_date.gte=${desdeAno}-01-01&primary_release_date.lte=${hastaAno}-12-12&vote_count.gte=1000&vote_average.gte=${puntaje}`
+
+  return fetch(url)
+    .then((r) => r.json())
+    .then((resultadoPeliculas) => mostrarPeliculas(resultadoPeliculas.results));
 }
 
-const form = document.querySelector('form');
-form.addEventListener('submit', obtenerPeliculas)
+function mostrarPeliculas(peliculas) {
+  console.log(peliculas);
+  const peliculasDOM = document.querySelector('#peliculas');
+  peliculasDOM.innerHTML = '';
 
+
+  peliculas.forEach((pelicula) => {
+    const titulo = pelicula.original_title;
+    const resumen = pelicula.overview;
+    const tapa = pelicula.poster_path;
+    const release = pelicula.release_date;
+    const puntaje = pelicula.vote_average;
+    const peliculasContainer = document.querySelector('#peliculas');
+
+    //Crea la parte de la imagen de la CARD
+    const cardImgContainer = document.createElement('div');
+    cardImgContainer.classList.add('card', 'mb-3');
+    const cardRow = document.createElement('div');
+    cardRow.classList.add('row', 'no-gutters');
+    cardImgContainer.appendChild(cardRow);
+    const cardImgColumna = document.createElement('div');
+    cardImgColumna.classList.add('col-md-4');
+    cardRow.appendChild(cardImgColumna);
+    const imagen = document.createElement('img');
+    imagen.classList.add('card-img');
+    imagen.setAttribute('src', `https://image.tmdb.org/t/p/original${tapa}`)
+    cardImgColumna.appendChild(imagen);
+
+    //Crea la parte de la info de la CARD
+    const cardInfoColumna = document.createElement('div');
+    cardInfoColumna.classList.add('col-md-8');
+    cardRow.appendChild(cardInfoColumna);
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    cardInfoColumna.appendChild(cardBody);
+    const cardTitulo = document.createElement('h5');
+    cardTitulo.classList.add('card-title');
+    cardBody.appendChild(cardTitulo);
+    const descripcion = document.createElement('p');
+    descripcion.classList.add('card-text');
+    descripcion.textContent = resumen;
+    cardBody.appendChild(descripcion);  
+
+    peliculasDOM.appendChild(cardImgContainer)
+  })
+
+
+}
 
 function configurarPagina() {
+  const form = document.querySelector('form');
   const anoDesde = document.querySelector('#anodesde');
   const anoHasta = document.querySelector('#anohasta');
   mostrarAnos(anoDesde);
   mostrarAnos(anoHasta);
   obtenerGeneros();
+  
+  form.addEventListener('submit', obtenerPeliculas)
 }
 
 configurarPagina()
