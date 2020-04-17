38f6bbc99345b593173eca53f6505b8c413a0f3f
Changuitoss <ezealvarezrubio@gmail.com>
2020-04-17 18:54:32 -0300

    No va a la pagina 0

--- src/ui.js
+++ src/ui.js
@@ -104,6 +104,7 @@ export function mostrarNavegacionPaginas(url, pagina, paginasTotales, tipo) {
     let urlNuevaIngles;
     nav.addEventListener('click', (e) => {
       const boton = e.target.dataset.boton;
+      console.log('pagina: ', pagina)
       
       if (boton == 'proxima' && pagina < paginasTotales) {
         pagina += 1;
@@ -116,7 +117,7 @@ export function mostrarNavegacionPaginas(url, pagina, paginasTotales, tipo) {
         .then(() => obtenerInfoPaginaIngles(urlNuevaIngles, tipo));
 
       } 
-      else if (boton == 'anterior') {
+      else if (boton == 'anterior' && pagina > 1) {
         pagina -= 1;
         peliculasNavNumero.forEach((nav) => {
           nav.textContent = pagina;
